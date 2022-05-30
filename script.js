4c639d65c6210f0441de237503950223ff13c5ae
Will <wilfredkorir46@gmail.com>
2022-05-30 14:29:03 +0300

    Added to main

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        if (this.scrollY > 300) {
+            $('.scroll-up-btn').addClass("show");
+        } else
+            $('.scroll-up-btn').removeClass("show")
+
+    });
+
+    //slide-up script
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 })
+    });
+    //toogle menu/navbar script
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.navbar i').toggleClass("active");
+    })
+});
